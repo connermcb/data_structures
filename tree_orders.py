b27dc8f21e215098ae94937db1144def2e978303
connermcb <connermcb@yahoo.com>
2017-05-31 22:12:30 -0500

    Correct base cases in all three order functions

--- tree_orders.py
+++ tree_orders.py
@@ -12,7 +12,7 @@ class Tree(object):
     
     def __init__(self, n):
         # list values = keys for self, dictkey left, dictkey right respectively
-        self.node_dict = {x:[-1, -1, -1]}
+        self.node_dict = {x:[-1, -1, -1] for x in range(n)}
         self.result_in_order = []
         self.result_pre_order = []
         self.result_post_order = []
@@ -23,27 +23,27 @@ class Tree(object):
 
                       
     def in_order(self, t):
-        if sum(self.node_dict[t][1:3]) == -2:
-            return
         key, left, right = self.node_dict[t][:3]
-        self.in_order(left)
+        if left != -1:
+            self.in_order(left)
         self.result_in_order.append(key)
-        self.in_order(right)
+        if right != -1:
+            self.in_order(right)
         
-    def pre_order(self, t):
-        if sum(self.node_dict[t][1:3]) == -2:
-            return       
+    def pre_order(self, t):     
         key, left, right = self.node_dict[t][:3]
         self.result_pre_order.append(key)
-        self.pre_order(left)
-        self.pre_order(right)
+        if left != -1:
+            self.pre_order(left)
+        if right != -1:
+            self.pre_order(right)
         
-    def post_order(self, t):
-        if sum(self.node_dict[t][1:3]) == -2:
-            return       
+    def post_order(self, t): 
         key, left, right = self.node_dict[t][:3]
-        self.post_order(left)
-        self.post_order(right)
+        if left != -1:
+            self.post_order(left)
+        if right != -1:
+            self.post_order(right)
         self.result_post_order.append(key)
     
     
@@ -51,14 +51,18 @@ n=5
 t = Tree(n)
 
 
-l =[(4, 1, 2), (2, 3, 4), (5, -1, -1), (1, -1, -1), (3, -1, -1)]
+lst =[(4, 1, 2), (2, 3, 4), (5, -1, -1), (1, -1, -1), (3, -1, -1)]
 
-for i in range(len(l)):
-    k, l, r = each
+for i in range(len(lst)):
+    k, l, r = lst[i]
     t.add_node(i,k,l,r)
     
 print(t.node_dict)
-    
-    
+t.in_order(0) 
+print(t.result_in_order)
+t.pre_order(0)
+print(t.result_pre_order)
+t.post_order(0)
+print(t.result_post_order)
         
     
