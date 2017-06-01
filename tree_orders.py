e38887ca0bdd2744d657e195570ba6df49175233
connermcb <connermcb@yahoo.com>
2017-05-31 21:22:13 -0500

    Replace Node class with simpler hash table

--- tree_orders.py
+++ tree_orders.py
@@ -4,39 +4,18 @@ In-Order, Pre-Order, Post-Order Traversals
 """
 
 
-class Node(object):
-    
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
-        self.parent = None
-        
-    def set_left(self, left):
-        self.left = left
-        
-    def set_right(self, right):
-        self.right = right
-        
-    def set_parent(self, parent):
-        self.parent = parent
-        
-    def get_left(self):
-        return self.left
-    
-    def get_right(self):
-        return self.right
-    
-    def get_parent(self):
-        return self.parent
+
     
     
     
 class Tree(object):
     
-    def __init__(self):
-        self.node_list = []
+    def __init__(self, n):
+        # list values = keys for self, left, right, parent respectively
+        self.node_dict = {x:[-1, -1, -1, -1] for x in range(n)}
         
+        
+    
     
     
     
