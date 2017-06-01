619f86026bbc7423e39d270c13a598d6dd873f30
connermcb <connermcb@yahoo.com>
2017-05-31 23:18:02 -0500

    Set up TreeChecker class and borrow in_order() from tree_orders

---
+++ is_binary_search_tree.py
@@ -0,0 +1,27 @@
+#python3
+# -*- coding: utf-8 -*-
+"""
+Binary Tree or Not
+"""
+
+class TreeChecker(object):
+    
+    def __init__(self, n):
+        # list values = keys for self, dictkey left, dictkey right respectively
+        self.node_dict = {x:[-1, -1, -1] for x in range(n)}
+        self.in_order_list = []
+
+               
+    def add_node(self, i, k, l, r):
+        self.node_dict[i] = [k, l, r]
+                     
+    def in_order(self, t):
+        key, left, right = self.node_dict[t][:3]
+        if left != -1:
+            self.in_order(left)
+        self.in_order_list.append(key)
+        if self.in_order_list[-1] < self.in_order_list[-2]:
+            
+            return
+        if right != -1:
+            self.in_order(right)
